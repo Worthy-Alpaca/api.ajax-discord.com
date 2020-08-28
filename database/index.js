52f22981ccd802300112f5e3a35e842f1d5559ae
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-08-28 16:18:06 +0200

    improved database connection

--- database/index.js
+++ database/index.js
@@ -5,7 +5,10 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: process.env.DB_password,
-    database: process.env.DB_database
+    database: process.env.DB_database,
+    charset: "utf8mb4",
+    encoding: "utf8mb4_unicode_520_ci",
+    acquireTimeout: 1000000
 });
 
 module.exports = con;
