7d4d0944c8fef9d8d8abe26aeaf5444ca8cc06d1
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-09-16 22:38:24 +0200

    removed unneeded import

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const verify = require('./verifyToken');
-const {Client} = require('discord.js');
-const client = new Client();
 
 
 router.get('/', verify, (req, res) => {
