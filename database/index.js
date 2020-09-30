e26460f68cfcf0ed7c93641b58ea107c4d4a3f4d
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-09-30 16:22:16 +0200

    changed DB connection to pool connection

--- database/index.js
+++ database/index.js
@@ -21,7 +21,7 @@ var db_config = {
     acquireTimeout: 1000000
 };
 
-var con;
+/* var con;
 
 function handleDisconnect() {
     con = mysql.createConnection(db_config); // Recreate the connection, since
@@ -30,7 +30,7 @@ function handleDisconnect() {
     con.connect(function (err) {              // The server is either down
         if (err) {                                     // or restarting (takes a while sometimes).
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+            setTimeout(handleDisconnect(), 2000); // We introduce a delay before attempting to reconnect,
         }                                     // to avoid a hot loop, and to allow our node script to
                                             // process asynchronous requests in the meantime.
         console.log("Connected to Database");
@@ -54,6 +54,23 @@ function handleDisconnect() {
     });
 }
 
-handleDisconnect();
+handleDisconnect(); */
+
+var con = mysql.createPool(db_config);
+
+con.getConnection(function (err, con) {
+    if (err) {
+        connection.release();
+        console.log(' Error getting mysql_pool connection: ' + err);
+        throw err;
+    }
+    con.query("CREATE TABLE IF NOT EXISTS servers(id VARCHAR(20) NOT NULL UNIQUE, name TEXT NOT NULL, admin TEXT, moderator TEXT, greeting VARCHAR(512), channel TEXT, approved TEXT, startcmd TEXT, reports TEXT, auto_approved TEXT, server_greeting TEXT, prefix TEXT, kick_limit INT, ban_limit INT) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;")
+    con.query("CREATE TABLE IF NOT EXISTS ranks(rank_id VARCHAR(20) NOT NULL UNIQUE, server_id VARCHAR(20) NOT NULL, rank_name TEXT NOT NULL);")
+    con.query("CREATE TABLE IF NOT EXISTS login(server_id VARCHAR(255) NOT NULL UNIQUE, password TEXT NOT NULL);");
+    con.query("CREATE TABLE IF NOT EXISTS channels(server_id VARCHAR(255) NOT NULL, channel_id VARCHAR(255) NOT NULL UNIQUE, channel_name TEXT NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;");
+    con.query("CREATE TABLE IF NOT EXISTS roles(server_id VARCHAR(255) NOT NULL, role_id VARCHAR(255) NOT NULL UNIQUE, role_name TEXT NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;");
+    con.query("CREATE TABLE IF NOT EXISTS commands(name VARCHAR(512) NOT NULL, category VARCHAR(512) NOT NULL, description VARCHAR(1024) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;");
+    console.log("Connected to Database");
+})
 
 module.exports = con;
