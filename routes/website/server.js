abba2c5e6d45eac1d668bbf74ebc7bf4a2ed21a6
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-11 02:35:34 +0100

    added endpoint to get all channels and roles

--- routes/website/server.js
+++ routes/website/server.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const verify = require('../verifyRegister');
 //Import functions
 const functions = require('../../database/website_queries');
+const api = require('./functions/apifunctions');
 
 
 router.get('/getserver', verify, async (req, res) => {
-    
     const data = await functions.getcurrentserver(req.query.guildID);
     const server = {
         data,
@@ -39,6 +39,40 @@ router.get('/getserver', verify, async (req, res) => {
             err: data
         });
     }
+    res.end();
+})
+
+router.get('/getcomponents', verify, async (req, res) => {
+    const data = {
+        roles: await functions.getroles(req.query.guildID),
+        channels: await functions.getchannels(req.query.guildID)
+    }
+    
+    if (data || typeof data == 'number') {
+        console.log("general get success")
+        //res.headers.append('Access-Control-Allow-Origin', '*');
+        res.status(200).json(data);
+    } else if (data === false) {
+        res.status(200).json({
+            status: 200,
+            success: false,
+            err: 'no data'
+        });
+    } else {
+        console.log("general get error");
+        res.status(409).json({
+            status: 409,
+            success: false,
+            err: data
+        });
+    }
+    res.end();
+})
+
+router.put('/test', verify, async (req, res) => {
+    console.log("#################################")
+    console.log(req.body)
+    res.status(200);
     res.end();
 })
 
