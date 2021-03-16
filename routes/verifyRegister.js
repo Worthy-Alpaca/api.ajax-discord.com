2a4c67350912af8a610d2c5e4267680b354d05fa
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-16 15:25:33 +0100

    added auth dev mode

--- routes/verifyRegister.js
+++ routes/verifyRegister.js
@@ -6,19 +6,38 @@ module.exports = function (req, res, next) {
         return res.status(400).send('Access denied');
     }
     //console.log("verify");
-    try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET, {maxAge: "1m"});
-        req.user = verified;
-        next();
-    } catch (err) {
-        //console.log("login redirect")
-        if (err.name === "TokenExpiredError") {
-            return res.status(400).json({
-                status: 400,
-                success: false,
-                err: err
-            })
+    // enabling dev mode
+    if (process.env.DEV === "true") {
+        try {
+            const verified = jwt.verify(token, process.env.TOKEN_SECRET, { ignoreExpiration: true });
+            req.user = verified;
+            next();
+        } catch (err) {
+            //console.log("login redirect")
+            if (err.name === "TokenExpiredError") {
+                return res.status(400).json({
+                    status: 400,
+                    success: false,
+                    err: err
+                })
+            }
+            res.status(400).send('Invalid Token!!');
         }
-        res.status(400).send('Invalid Token!!');
+    } else {
+        try {
+            const verified = jwt.verify(token, process.env.TOKEN_SECRET, { maxAge: "1m" });
+            req.user = verified;
+            next();
+        } catch (err) {
+            //console.log("login redirect")
+            if (err.name === "TokenExpiredError") {
+                return res.status(400).json({
+                    status: 400,
+                    success: false,
+                    err: err
+                })
+            }
+            res.status(400).send('Invalid Token!!');
+        }
     }
 }
