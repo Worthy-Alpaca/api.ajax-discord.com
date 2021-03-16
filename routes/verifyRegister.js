8df8c618b02bf64ed0422ffb97d74443d2bd37f1
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-16 15:19:17 +0100

    added token expiration check

--- routes/verifyRegister.js
+++ routes/verifyRegister.js
@@ -7,11 +7,18 @@ module.exports = function (req, res, next) {
     }
     //console.log("verify");
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET, {maxAge: "1m"});
         req.user = verified;
         next();
-    } catch {
+    } catch (err) {
         //console.log("login redirect")
+        if (err.name === "TokenExpiredError") {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                err: err
+            })
+        }
         res.status(400).send('Invalid Token!!');
     }
 }
